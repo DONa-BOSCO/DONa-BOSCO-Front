a085283139d880de4e3a2032ba7aac9b29f2e453
Rocio-Leiva <rmleipec@gmail.com>
2023-04-05 12:22:43 +0200

    added form add product

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { productHandler } from '../handlers/productHandler';
 
 
-const AddProduct = () => {
+const CreateProduct = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [img, setImg] = useState(null);
@@ -15,7 +15,7 @@ const AddProduct = () => {
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
   };
- 
+
   const handleDescriptionChange = (event) => {
     setDescription(event.target.value);
   };
@@ -44,7 +44,7 @@ const AddProduct = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    let newProduct = { title, description,  img,  condition, location, category };
+    let newProduct = { title, description, img, condition, location, category };
     productHandler.addProduct(newProduct);
     event.target.reset()
   };
@@ -54,48 +54,118 @@ const AddProduct = () => {
   const handleShow = () => setShow(true);
 
   return (
-    <>
-      {/* <SideBarLogged /> */}
-      <div className="container-form">
-        <form onSubmit={handleSubmit} itemID="form1" enctype="multipart/form-data">
-          <h1>Create a new product</h1>
-
-          <div className="mb-3">
-            <label htmlFor="title" className="form-label">Title</label>
-            <input name="title" type="text" className="form-control" placeholder="Product name" onChange={handleTitleChange} required />
-          </div>
 
 
-          <div className="mb-3">
-            <label className="form-label" htmlFor="description">Description</label>
-            <input name="description" type="text" className="form-control" id="input-description" required placeholder="Tell us about the product." onChange={handleDescriptionChange} />
-          </div>
+    <div className="container-form">
+      <form onSubmit={handleSubmit} itemID="form1" enctype="multipart/form-data">
+        <h1>Información del Producto</h1>
 
-          <div className="mb-3">
-            <label className="form-label" htmlFor="Condition">Condition</label>
-            <input name="Condition" type="text" className="form-control" id="input-Condition" required placeholder="What Condition is it made of?" onChange={handleConditionChange} />
-          </div>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">Nombre</label>
+          <input name="title" type="text" className="form-control" id="input-Title" placeholder="Producto que vas a donar" onChange={handleTitleChange} required />
+        </div>
 
-          <div className="mb-3">
-            <label className="form-label" htmlFor="Location">Location</label>
-            <input name="Location" type="text" className="form-control" id="input-Location" required placeholder="Type of Location." onChange={handleLocationChange} />
-          </div>
 
-          <div className="mb-3">
-            <label className="form-label" htmlFor="category">Category</label>
-            <input name="category" type="text" className="form-control" id="input-category" required placeholder="What is the category?" onChange={handleCategoryChange} />
-          </div>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="description">Descripción</label>
+          <input name="description" type="text" className="form-control" id="input-description" placeholder="Breve describe el producto" onChange={handleDescriptionChange} maxLength="150" required />
+        </div>
 
-         
-          <div className="mb-3">
-            <label htmlFor="img" className="form-label">Image</label>
-            <input name="img" type="file" className="form-control" placeholder="Upload a picture" onChange={handleImgChange} required />
-          </div>
 
-          <button type="submit" className="btn btn-primary" id="btn" onClick={handleShow}>Upload</button>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="condition">Estado del producto</label>
+          <select name="condition" className="form-select" onChange={handleConditionChange} required>
+            <option value="">Selecciona una de la 3 opciones</option>
+            <option value="nuevo">Nuevo</option>
+            <option value="seminuevo">Seminuevo</option>
+            <option value="usado">Usado</option>
+          </select>
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="Location">¿Dónde se encuentra el producto?</label>
+          <select name="Location" className="form-select" onChange={handleLocationChange} required>
+            <option value="">Seleccionar provincia</option>
+            <option value="Álava">Álava</option>
+            <option value="Albacete">Albacete</option>
+            <option value="Alicante">Alicante</option>
+            <option value="Almería">Almería</option>
+            <option value="Asturias">Asturias</option>
+            <option value="Ávila">Ávila</option>
+            <option value="Badajoz">Badajoz</option>
+            <option value="Barcelona">Barcelona</option>
+            <option value="Burgos">Burgos</option>
+            <option value="Cáceres">Cáceres</option>
+            <option value="Cádiz">Cádiz</option>
+            <option value="Cantabria">Cantabria</option>
+            <option value="Castellón">Castellón</option>
+            <option value="Ceuta">Ceuta</option>
+            <option value="Ciudad Real">Ciudad Real</option>
+            <option value="Córdoba">Córdoba</option>
+            <option value="Cuenca">Cuenca</option>
+            <option value="Gerona">Gerona</option>
+            <option value="Granada">Granada</option>
+            <option value="Guadalajara">Guadalajara</option>
+            <option value="Guipúzcoa">Guipúzcoa</option>
+            <option value="Huelva">Huelva</option>
+            <option value="Huesca">Huesca</option>
+            <option value="Islas Baleares">Islas Baleares</option>
+            <option value="Jaén">Jaén</option>
+            <option value="La Coruña">La Coruña</option>
+            <option value="La Rioja">La Rioja</option>
+            <option value="Las Palmas">Las Palmas</option>
+            <option value="León">León</option>
+            <option value="Lérida">Lérida</option>
+            <option value="Lugo">Lugo</option>
+            <option value="Madrid">Madrid</option>
+            <option value="Málaga">Málaga</option>
+            <option value="Melilla">Melilla</option>
+            <option value="Murcia">Murcia</option>
+            <option value="Navarra">Navarra</option>
+            <option value="Orense">Orense</option>
+            <option value="Palencia">Palencia</option>
+            <option value="Pontevedra">Pontevedra</option>
+            <option value="Salamanca">Salamanca</option>
+            <option value="Santa Cruz de Tenerife">Santa Cruz de Tenerife</option>
+            <option value="Segovia">Segovia</option>
+            <option value="Sevilla">Sevilla</option>
+            <option value="Soria">Soria</option>
+            <option value="Tarragona">Tarragona</option>
+            <option value="Teruel">Teruel</option>
+            <option value="Toledo">Toledo</option>
+            <option value="Valencia">Valencia</option>
+            <option value="Valladolid">Valladolid</option>
+            <option value="Vizcaya">Vizcaya</option>
+            <option value="Zamora">Zamora</option>
+            <option value="Zaragoza">Zaragoza</option>
+
+          </select>
+        </div>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="category">Categoría</label>
+          <select name="category" className="form-select" onChange={handleCategoryChange} required>
+            <option value="">Selecciona la categoría del producto</option>
+            <option value="Ropa y accesorios">Ropa y accesorios</option>
+            <option value="electrodomesticos">Electrodomésticos</option>
+            <option value="Informatica y electrónica">Dispositivos electrónicos</option>
+            <option value="Deporte y ocio">Deporte y ocio</option>
+            <option value="videojuegos y videojuegos"> videojuegos y juegos</option>
+            <option value="cine, libros y musica">cine, libros y música</option>.
+            <option value="Hogar y jardín">Hogar y jardín</option>
+            <option value="articulos infantiles">Artículos infantiles</option>
+            <option value="otros">otros</option>
+          </select>
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="img" className="form-label">Foto</label>
+          <input name="img" type="file" className="form-control" placeholder="Upload a picture" onChange={handleImgChange} required />
+        </div>
+
+        <button type="submit" className="btn btn-primary" id="btn" onClick={handleShow}>Subir</button>
       </form>
     </div>
-    </>
+
   );
 };
-export default AddProduct;
+export default CreateProduct;
