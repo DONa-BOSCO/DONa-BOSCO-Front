6e927f183ae48d9da35dfe8901e249022bd481af
miriamremesal <miriamremesal@gmail.com>
2023-04-05 12:48:10 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-const Footer = () => {
+function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <Navbar className='footer'>
+        <Container>
+          <Navbar.Brand>Copyright© 2023 - DONa BOSCO</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="#Aviso">Aviso Legal</Nav.Link>
+            <Nav.Link href="#Privacidad">Política de Privacidad</Nav.Link>
+            <Nav.Link href="#Cookies">Cookies</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
-export default Footer
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import 'react-bootstrap' 
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
