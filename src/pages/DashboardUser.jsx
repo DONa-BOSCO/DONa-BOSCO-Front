d62875838764f4cc39ff1332ae08d4dcff268e63
Rocio-Leiva <rmleipec@gmail.com>
2023-04-06 19:42:19 +0200

    added style to card

--- src/pages/DashboardUser.jsx
+++ src/pages/DashboardUser.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { useLoaderData } from "react-router-dom";
-import { BsSearchHeart, BsCart3 } from 'react-icons/bs';
+import { BsEye } from 'react-icons/bs';
 import ProductModalUser from '../components/ProductModalUser';
 import './Stylesheet/DashboardUser.css';
 import { productService } from "../services/productService.js";
 
 function Dashboard() {
- 
+
   const [productsData, setProductsData] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("");
 
@@ -45,16 +45,16 @@ function Dashboard() {
     cartProducts.push(product);
     localStorage.setItem("cartProducts", JSON.stringify(cartProducts));
   }
-  
+
   useEffect(() => {
     getProducts()
-  },[])
+  }, [])
 
   return (
     <>
       <div className="container-gn" id="text">
         <br />
-        <h1> Todos los Productos</h1>
+        <h1> <b>Todos los Productos</b></h1>
         <div className="container-bar">
           <input
             className="searchStyle"
@@ -64,49 +64,35 @@ function Dashboard() {
             onChange={handleCategoryChange}
           />
         </div>
-      </div>
 
-      <div className="cards">
-        {data.map((product) => {
-          return (
-   // <React.Fragment key={product.id}>
-            //   <ProductModalUser 
-            //     show={show}
-            //     handleClose={handleClose}
-            //     product={productModal}
-            //   />
-            //   <Card border="light" style={{ width: '18rem' }}>
-            //     <Card.Img variant="top" src={product.img} />
-            //     <Card.Body className='text-center'>
-            //       <Card.Title>{product.title} </Card.Title>
-            //       <Card.Subtitle className="mb-2 text-muted">{product.category}</Card.Subtitle>
-            //       <Card.Subtitle className="mb-2 text-muted">${product.description}</Card.Subtitle>
-            //       <Card.Subtitle className="mb-2 text-muted">${product.condition}</Card.Subtitle>
-            //       <Card.Subtitle className="mb-2 text-muted">${product.location}</Card.Subtitle>
-            //       <Button variant="light" onClick={() => handleShow(product.id)}><BsSearchHeart />Ver Producto</Button>
-                
-            //     </Card.Body>
-            //   </Card>
-            // </React.Fragment>
-            <>
-            
-            <ProductModalUser show={show} handleClose={handleClose} productModal={productModal} />
-              <Card border="light" style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={buildImg(product.base64FileModel.extension, product.base64FileModel.content)} />
-                    <Card.Body className='text-center'>
-                      <Card.Title>{product.producItem.title} </Card.Title>
-                      <Card.Subtitle className="mb-2 text-muted">{product.producItem.category}</Card.Subtitle>
-                      <Card.Subtitle className="mb-2 text-muted">${product.producItem.description}</Card.Subtitle>
-                      <Card.Subtitle className="mb-2 text-muted">${product.producItem.condition}</Card.Subtitle>
-                      <Card.Subtitle className="mb-2 text-muted">${product.producItem.location}</Card.Subtitle>
-                    <Button variant="light" onClick={() => handleShow(product.producItem.id)}><BsSearchHeart/> Ver </Button>
-                 
-                    </Card.Body>
-              </Card>
-          </>
-         
-          )
-        })}
+
+        <div className="cards">
+          {data.map((product) => {
+            return (
+
+              <>
+
+                <ProductModalUser show={show} handleClose={handleClose} productModal={productModal} />
+                <Card border="light" style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={buildImg(product.base64FileModel.extension, product.base64FileModel.content)} />
+                  <Card.Body>
+                    <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
+                      <Card.Title style={{ marginRight: '1rem' }}><b>{product.producItem.title}</b></Card.Title>
+                      <Button variant="light" onClick={() => handleShow(product.producItem.id)}><BsEye /> Ver </Button>
+                    </div>
+                    <Card.Subtitle className="mb-2 text-muted">{product.producItem.category}</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">{product.producItem.description}</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">{product.producItem.condition}</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">{product.producItem.location}</Card.Subtitle>
+
+
+                  </Card.Body>
+                </Card>
+              </>
+
+            )
+          })}
+        </div>
       </div>
     </>
   );
