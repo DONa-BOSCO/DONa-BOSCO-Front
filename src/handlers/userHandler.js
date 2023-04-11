9a6f23fcfa39a0241149a461f7986f3029f8b8d1
Rocio-Leiva <rmleipec@gmail.com>
2023-04-11 18:56:20 +0200

    added userService and userHandler

---
+++ src/handlers/userHandler.js
@@ -0,0 +1,71 @@
+import { userService } from '../services/userService';
+
+export const userHandler = {
+    addUser(newUser) {
+        if (!newUser) {
+            return;
+        }
+
+        // let imgStringData = newUser.img;
+        // let imgStringDataSplit = imgStringData.split(',');
+        // let imgContent = imgStringDataSplit[1];
+
+        let newUserRequestModel = {
+            "userData": {
+                "rol": 2,
+                "fullName": newUser.fullName,
+                "nickName": newUser.nickName,
+                "email": newUser.email,
+                "password": newUser.password,
+                "age": newUser.age,
+                "phoneNumber": newUser.phoneNumber,
+            },
+
+            "fileData": {
+                "fileName": newUser.fullName + "-Photo",
+                "base64FileContent": imgContent
+            }
+
+        }
+
+        return userService.addUser(newUserRequestModel);
+
+    },
+
+    async loadUser() {
+        var result = await userService.getUser();
+        return result;
+    },
+    loadUser(id) {
+        return userService.getUser(id);
+    },
+    deleteUser(id) {
+        return userService.deleteUser(id);
+    },
+    updateUser(id, updatedUser) {
+        if (!updatedUser) {
+            return;
+        }
+
+        let imgStringData = updatedUser.img;
+        let imgStringDataSplit = imgStringData.split(',');
+        let imgContent = imgStringDataSplit[1];
+
+        let updatedUserRequestModel = {
+            "userData": {             
+                "fullName": updatedProduct.fullName,
+                "nickName": updatedProduct.nickName,
+                "email": updatedProduct.email,
+                "password": updatedProduct.password,
+                "age": updatedProduct.age,
+                "phoneNumber": updatedProduct.phoneNumber,
+            },
+            "fileData": {
+                "fileName": newUser.fullName + "-Photo",
+                "base64FileContent": imgContent
+            }
+        }
+
+        return userService.updateUser(id, updatedUserRequestModel);
+    }
+}
---
+++ src/services/userService.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import Swal from 'sweetalert2';
+
+const apiClient = axios.create({
+    baseURL: 'https://localhost:7048/',
+    withCredentials: false,
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'   
+    }
+})
+
+export const userService = {
+    async getUser() {
+        let response = await apiClient.get("/User/GetAllUsers");
+        if (!response==200)
+            {
+                alert('Hubo un error al buscar estos usuarios');
+            };
+        let allUser = response.data;
+       
+        return allUser;
+    },
+    async getUser(id) {
+        let response = await apiClient.get("/" + id);
+        if (!response == 200)
+            throw {
+                status: response.status,
+                statusText: "Not found",
+            };
+        let user = response.data;
+        return user;
+    },
+    async addUser(newUserRequestModel){
+        let response = await apiClient.post("/User/InsertUser", newUserRequestModel)
+        if (response.status === 200) 
+        {   
+            Swal.fire({ 
+            icon: 'success', 
+            title: 'Nuevo Usuario Registrado', 
+            showConfirmButton: true, 
+            showClass: { popup: 'animate__animated animate__fadeInDown' }, 
+            hideClass: { popup: 'animate__animated animate__fadeOutUp' } })   
+         } else { 
+            alert("Upsi...");
+         }
+
+    },
+    async deleteUser(id) {
+        await apiClient.delete("/User/DeactivateUser?id=" + id)
+    },
+    async updateUser(id, updatedUserRequestModel){
+        await apiClient.patch("/User/Patch" + id, updatedUserRequestModel)
+        if(response.status === 200)
+        {
+            Swal.fire({
+            icon: 'success',
+            title: 'el usuario ha sido actualizado',
+            showConfirmButton: true,
+            showClass: { popup: 'animate__animated animate__fadeInDown' },
+            hideClass: { popup: 'animate__animated animate__fadeOutUp' } })
+         }
+    }
+
+    }
