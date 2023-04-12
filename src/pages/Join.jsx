50ec6f3a4ec37f25fb7e6fee8c6b27a4cdbfff67
VeronicaAnais <veroangap@hotmail.es>
2023-04-12 11:28:33 +0200

    ultimate changes

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,65 +1,40 @@
-import React from 'react'
+import React from 'react';
 import { Form, Button } from 'react-bootstrap';
+
 const Join = () => {
   return ( 
-    <>
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="col-md-6">
+        <Form>
+          <div className="mb-3">
+            <Form.Label>Nombre y Apellido</Form.Label>
+            <Form.Control type="Nombre y Apellido" placeholder="nombre y apellido" />
+          </div>
+          <div className="mb-3">
+            <Form.Label>Nombre de Usuario</Form.Label>
+            <Form.Control type="Nombre de Usuario" placeholder="nombre de usuario" />
+          </div>
+          <div className="mb-3">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="Email" placeholder="email" />
+          </div>
+          <div className="mb-3">
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control type="Contraseña" placeholder="contraseña" />
+          </div>
+          <Form.Group className="mb-3">
+            <Form.Check
+              required
+              label="He leído y acepto las condiciones"
+              feedback="You must agree before submitting."
+              feedbackType="invalid"
+            />
+          </Form.Group>
+          <center><Button variant="danger">Añadir Cuenta</Button></center>
+        </Form>
+      </div>
+    </div>
+  );
+};
 
-      <form>
-        
-      <div class="container mt-5 p-5">
-
-        
-        <div className="mb-3">
-          <label>Nombre y Apellido</label>
-          <input
-            type="Nombre y Apellido"
-            className="form-control"
-            placeholder="nombre y apellido"
-          />
-        </div>
-        <div className="mb-3">
-          <label>Nombre de Usuario</label>
-          <input
-            type="Nombre de Usuario"
-            className="form-control"
-            placeholder="nombre de usuario"
-          />
-        </div>
-        <div className="mb-3">
-          <label>Email</label>
-          <input
-            type="Email"
-            className="form-control"
-            placeholder="email"
-          />
-        </div>
-        <div className="mb-3">
-          <label>Contraseña</label>
-          <input
-            type="Contraseña"
-            className="form-control"
-            placeholder="contraseña"
-          />
-        </div>
-        <Form.Group className="mb-3">
-        <Form.Check
-          required
-          label="He leído y acepto las condiciones"
-          feedback="You must agree before submitting."
-          feedbackType="invalid"
-        />
-      </Form.Group>
-       
-       
-      <Button variant="danger">Añadir Cuenta</Button>
-     
-       </div>
-      </form>
-    
-
-
-    
-    </>
-  )
-}
-export default Join
+export default Join;
