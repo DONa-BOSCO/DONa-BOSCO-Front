163a33ce6dade910c61d9b83b0a1123e1c519698
CLAUDETTEGONZALEZ <claudettegonzalezf5@gmail.com>
2023-04-13 11:33:19 +0200

    footer color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import '../components/stylesheet/Footer.css'
+
 
 function Footer() {
   return (
     <>
-      <Navbar className='footer'>
+      <Navbar className="footer fixed-bottom">
         <Container>
           <Navbar.Brand>Copyright© 2023 - DONa BOSCO</Navbar.Brand>
           <Nav>
-            <Nav.Link href="#Aviso">Aviso Legal</Nav.Link>
-            <Nav.Link href="#Privacidad">Política de Privacidad</Nav.Link>
+            <Nav.Link href="#Aviso">Aviso Legal |</Nav.Link>
+            <Nav.Link href="#Privacidad">Política de Privacidad |</Nav.Link>
             <Nav.Link href="#Cookies">Cookies</Nav.Link>
           </Nav>
         </Container>
