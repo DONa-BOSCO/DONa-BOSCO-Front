b286b7f12a3c9db25b9c01f4b8c08ad542f2258b
VeronicaAnais <veroangap@hotmail.es>
2023-04-18 09:55:41 +0200

    responsive changes

--- src/components/Sidebarc.jsx
+++ src/components/Sidebarc.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Nav } from "react-bootstrap";
 import { AiOutlineHeart, AiOutlineTags } from "react-icons/ai";
 import { BsFillPersonFill } from "react-icons/bs";
-import "../components/styleSheet/SideBarc.css";
 
 const Sidebarc = () => {
   return (
@@ -10,21 +9,35 @@ const Sidebarc = () => {
       <div className="row">
         <div className="bg-danger col-3 min-vh-100 d-flex flex-column flex-grow-0">
           <hr />
-          <Nav.Link href="/mi-perfil" className="text-dark fs-5 fs-md-4 fs-lg-3 mb-5">
-            <i className="bi bi-speedometer2"></i>
-           <center> <BsFillPersonFill />
-            <span className="ms-2">Mi Perfil</span></center>
+
+          <Nav.Link
+            href="/mi-perfil"
+            className="text-dark fs-5 fs-md-4 fs-lg-3 mb-5">
+            <center>
+              <BsFillPersonFill />
+              <span className="ms-2 d-none d-sm-inline">Mi Perfil</span>
+            </center>
           </Nav.Link>
-          <Nav.Link href="/productos" className="text-dark fs-5 fs-md-4 fs-lg-3 mb-5">
-            <i className="bi bi-house"></i>
-            <center> <AiOutlineTags />
-            <span className="ms-2">Por Donar</span></center>
+
+          <Nav.Link
+            href="/productos"
+            className="text-dark fs-5 fs-md-4 fs-lg-3 mb-5">
+            <center>
+              <AiOutlineTags />
+              <span className="ms-2 d-none d-sm-inline">Por Donar</span>
+            </center>
           </Nav.Link>
-          <Nav.Link href="/ya-donados" className="text-dark fs-5 fs-md-4 fs-lg-3 mb-5">
-            <i className="bi bi-table"></i>
-            <center><AiOutlineHeart />
-            <span className="ms-2">Donados</span></center>
+
+          <Nav.Link
+            href="/ya-donados"
+            className="text-dark fs-5 fs-md-4 fs-lg-3 mb-5">
+            <center>
+              <AiOutlineHeart />
+              <span className="ms-2 d-none d-sm-inline">Donados</span>
+            </center>
           </Nav.Link>
+
+          
         </div>
       </div>
     </div>
