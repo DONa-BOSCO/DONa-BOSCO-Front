b81ec64082a7d551084560f40759f9cd44537fda
AldaraMG~ <almaga71@gmail.com>
2023-04-21 13:20:19 +0200

    added logout

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+
+
 import { NavLink } from 'react-router-dom';
 import { AiOutlineHeart, AiOutlineTags } from "react-icons/ai";
 import { BsFillPersonFill } from "react-icons/bs";
 import "./Stylesheet/Sidebar.css"
+import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
+
 
 const Sidebar = () => {
+    
+    const [isLoggedIn, setLoggedIn] = useState(true);
+    const handleLogout = () => {
+        localStorage.removeItem('nombreDelToken');
+        setLoggedIn(false);
+        window.location.href = '/login';
+      };
+
   return (
     <div className="sidebar"  style={{position: 'fixed'}}>
       <NavLink activeClassName="active" exact to="/miPerfil">
@@ -16,6 +28,7 @@ const Sidebar = () => {
       <NavLink activeClassName="active" to="/yaDonados">
       <AiOutlineHeart /> Ya Donados
       </NavLink>
+      <Button className=" btn btn-custom"onClick={handleLogout}>Cerrar Sesión</Button>
     </div>
   );
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -89,4 +89,6 @@ export const loginUser = async(data) => {
         })
     } 
     return dataResponse;
+    
 };
+
