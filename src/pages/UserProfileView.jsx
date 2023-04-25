0ec2ae1abc86d64708da46286c3985a25ae797da
VeronicaAnais <veroangap@hotmail.es>
2023-04-25 14:11:34 +0200

    functions

--- src/pages/UserProfileView.jsx
+++ src/pages/UserProfileView.jsx
@@ -6,8 +6,11 @@ import { Button } from "react-bootstrap";
 const UserProfile = () => {
 
     const [name, setName] = useState("");
-    const [last_Name, setLast_Name] = useState("");
-    const [phone_Number, setPhone_Number] = useState("");
+    const [nick_Name, setNick_Name] = useState("");
+    const [email, setEmail] = useState("");
+    const [description, setDescription] = useState("");
+
+
 
 
 
@@ -15,20 +18,42 @@ const UserProfile = () => {
         let nameInput = event.target.value;
         setName(nameInput);
     };
-    const handleLast_NameChange = (event) => {
-        let last_NameInput = event.target.value;
-        setLast_Name(last_NameInput);
+    const handleNick_NameChange = (event) => {
+        let nick_NameInput = event.target.value;
+        setNick_Name(nick_NameInput);
     };
-    const handlePhone_NumberChange = (event) => {
-        let phone_NumberInput = event.target.value;
-        setPhone_Number(phone_NumberInput);
+    const handleEmail_EmailChange = (event) => {
+        let emailInput = event.target.value;
+        setEmail(emailInput);
     };
+    const handleDescription_DescriptionChange = (event) => {
+        let descriptionInput = event.target.value;
+        setDescription(descriptionInput);
+    };
+
+    const handleImageChange = (event) => {
+        const image = event.target.files[0];
+        const formData = new FormData();
+        formData.append('image', image);
+
+        fetch('/api/upload-image', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => {
+                // handle response from server
+            })
+            .catch(error => {
+                // handle error
+            });
+    }
+
 
 
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        let newUserProfile = { name, last_Name, phone_Number, };
+        let newUserProfile = { name, nick_Name, email, description };
         userProfileHandler.addUserProfile(newUserProfile);
         event.target.reset()
     };
@@ -49,28 +74,30 @@ const UserProfile = () => {
                         <p>Información</p>
                     </div>
                     <div className="row">
-                        <div className="col-md-5 text-dark" >
+                        <div className="col-md-5 text-dark fs-4 fs-md-5" >
 
-                            <div className="profile d-flex flex-column align-items-center">
-                                <img src="https://user-images.githubusercontent.com/117834362/233957884-8aa4ced0-6526-4d35-b74e-d31b6dcaa667.jpg" className="rounded-circle" style={{ width: 180 }}></img>
-                                <Button className="btn btn-custom  mt-3" type="submit" id="btn" >Subir Nueva Imagen</Button>
+                            <div className="profile d-flex flex-column align-items-center fs-4 fs-md-5">
+
+                                <img src="https://user-images.githubusercontent.com/117834362/233957884-8aa4ced0-6526-4d35-b74e-d31b6dcaa667.jpg" className="rounded-circle fs-4 fs-md-5" style={{ width: 180 }}></img>
+                                <Button className="btn btn-custom mt-3 btn-sm mt-3 fs-10 fs-md-5"><input type="file" accept="image/*"  value="" onChange={handleImageChange} />Subir Nueva Imagen</Button>
+
                             </div>
 
                         </div>
-                        <div className="col-md-7">
+                        <div className="col-md-7 ">
 
 
                             <div>
-                                <div className="div1 bg-white text-muted"> <p>Nombre y Apellidos</p></div>
-                                <div className="div2 bg-white text-muted" ><p>Nombre de Usuario</p></div>
-                                <div className="div3 bg-white text-muted" ><p>Correo Electrónico</p></div>
-                                <div className="div3 bg-white text-muted" ><p>Descripción</p></div>
+                                <div className="name div1 bg-white text-muted fs-7 fs-md-5"> <p>Nombre y Apellidos</p></div>
+                                <div className="nick-Name div2 bg-white text-muted fs-7 fs-md-5" ><p>Nombre de Usuario</p></div>
+                                <div className="email div3 bg-white text-muted fs-7 fs-md-5" ><p>Correo Electrónico</p></div>
+                                <div className="description div3 bg-white text-muted fs-7 fs-md-5" ><p>Descripción</p></div>
                             </div>
                             <div className="d-grid gap-3">
                                 <div className="text-center">
 
 
-                                    <Button className="btn btn-custom btn-sm mt-3 text-center" type="submit" id="btn">Cerrar Sesión</Button>
+                                    <Button className="btn btn-custom btn-sm mt-3 fs-7 fs-md-5 text-center" type="submit" id="btn">Cerrar Sesión</Button>
                                 </div>
                             </div>
                         </div>
